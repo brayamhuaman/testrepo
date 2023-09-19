85135377ff1439c7948706eb6d710c5910dcc57c
Brayam Huaman <74569086+brayamhuaman@users.noreply.github.com>
2023-09-19 21:39:28 +0000

    ```

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react '
+
+function Home() {
+const listltems = [1,2, 3,4]
+const addltem = ( ) {
+const variableA = 1;
+const variableB = 3;
+const variableC = 2;
+return variableA*3 + variableC*variableB
+}
+return (
+<><div>Home</div>
+<p>hola mund0</p>
+<button onclick={addltem} /></>
+)
+export default Home
